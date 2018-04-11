c7fc90887844e29f6bbc31b70dd55a2b5a4b08a5
dswatson <david.s.watson11@gmail.com>
2018-04-11 16:00:02 +0200

    sim update

--- simulations.R
+++ simulations.R
@@ -1,31 +1,158 @@
-# Load libraries
+### SIMULATED DATA ###
+### Evaluate error rates and nominal coverage
+### Compare with and without precision weights
+### Test behavior on correlated predictors
+### Measure AUROC and AUPR vs. different VIMs
+
+# Load libraries, register cores
 library(mlbench)
-library(MASS)
-library(relaimpo)
+library(ranger)
+library(party)
 library(tidyverse)
+library(doMC)
+registerDoMC(4)
 
-# Consider a plot like Fig. 3 from Strobl et al. (2008):
-# x-axis is feature ID, y-axis is VI, and we see different color curves 
-# (with SE) for different measures of VI
+# Load cpi
+source('cpi.R')
 
-# Probably worth writing a stripped down version of c_imp that just spits out
-# deltas, then looping it with different seeds like 1k times to get a spread
-# Will also need 1k runs of gini, perm, and strobl_cond importance
+# Friedman1 benchmark
+n <- 100
+p <- 10
 
-# Friedman
-df <- as.data.frame(mlbench.friedman1(n = 100))
-out <- c_imp(df[, 1:10], df$y)
+# CPI loop
+loop <- function(b) {
+  
+  # Preliminaries
+  set.seed(b)
+  dat <- mlbench.friedman1(n = n)
+  colnames(dat$x) <- paste0('x', seq_len(p))
+  mtry <- floor(p / 3)
+  B <- 1000
+  ctrl <- cforest_unbiased(mtry = mtry, ntree = B)
+  
+  # t-test
+  cpi_t <- brute_force(dat$x, dat$y, test = NULL, weights = FALSE,
+                       replace = FALSE, mtry = mtry, B = B, 
+                       n.cores = 1, seed = b) 
+  
+  # Weighted t-test
+  cpi_wtd <- brute_force(dat$x, dat$y, test = 't', weights = TRUE,
+                         replace = FALSE, mtry = mtry, B = B, 
+                         n.cores = 1, seed = b) 
+  cpi_wtd <- cpi_wtd$CPI
+  
+  # MDI
+  f <- ranger(data = df, dependent.variable.name = 'y',
+              mtry = mtry, num.trees = B, replace = FALSE, 
+              importance = 'impurity', num.threads = 1, seed = b)
+  mdi <- importance(f)
+  
+  # MDA
+  f <- ranger(data = df, dependent.variable.name = 'y',
+              mtry = mtry, num.trees = B, replace = FALSE, 
+              importance = 'permutation', num.threads = 1, seed = b)
+  mda <- importance(f)
+  
+  # MDA-C
+  f <- cforest(y ~ ., data = df, controls = ctrl)
+  mda_c <- varimp(f, conditional = TRUE)
+  
+  # Altogether now
+  data.frame(
+    Feature = colnames(dat$x),
+    CPI = cpi_t,
+    CPI_wtd = CPI_wtd,
+    MDI = mdi,
+    MDA = mda
+    MDA_C = mda_c,
+    Run = b
+  )
+}
+sims <- foreach(b = seq_len(100), .combine = rbind) %dopar% loop(b) 
+saveRDS(sims, './Results/Simulations/Friedman1.rds')
 
-# Strobl et al.
+# RNA-seq simulation (from Love et al., 2014)
+library(DESeq2paper)
+library(DESeq2)
+source('./DESeq2paper/inst/script/makeSim.R')
+load('./DESeq2paper/data/meanDispPairs.RData')
+
+# Hyper-parameters
+n <- 100
+p <- 1e4
+lfc <- log2(2)
+grp <- factor(rep(c('A', 'B'), each = n / 2))
+x <- model.matrix(~ grp)
+
+loop <- function(b) {
+  
+  set.seed(b)
+
+  # Simulate counts
+  beta <- c(rep(0, p * 8/10), 
+            sample(c(-lfc, lfc), p * 2/10, replace = TRUE))
+  de_idx <- abs(beta) > 0
+  cnts <- makeSim(p, n, x, beta, meanDispPairs)$mat
+  
+  # Make DESeqDataSet object for normalization 
+  dds <- DESeqDataSetFromMatrix(cnts, colData = data.frame(grp), design = ~ grp)
+  dds <- estimateSizeFactors(dds)
+  mat <- t(counts(dds, normalized = TRUE))
+  dimnames(mat) <- list(paste0('s', seq_len(n)), paste0('x', seq_len(p)))
+  
+  # Remove singleton genes
+  keep <- rowSums(cnts) > 1
+  mat <- mat[, keep]
+  de_idx <- de_idx[keep]
+  
+  # Preliminaries
+  df <- data.frame(mat, y = grp)
+  p <- ncol(mat)
+  mtry <- floor(sqrt(p))
+  B <- 2e4
+  
+  # CPI
+  cpi <- rf_split(mat, grp, type = 'classification', test = NULL,
+                  mtry = mtry, B = B, replace = FALSE, 
+                  n.cores = 1, seed = b)
+  
+  # MDI
+  f <- ranger(data = df, dependent.variable.name = 'y',
+              mtry = mtry, num.trees = B, replace = FALSE, 
+              importance = 'impurity', num.threads = 1, seed = b,
+              classification = TRUE)
+  mdi <- importance(f)
+  
+  # MDA
+  f <- ranger(data = df, dependent.variable.name = 'y',
+              mtry = mtry, num.trees = B, replace = FALSE, 
+              importance = 'permutation', num.threads = 1, seed = b,
+              classification = TRUE)
+  mda <- importance(f)
+  
+  # Altogether now
+  data.frame(
+    Feature = colnames(mat),
+    CPI = cpi,
+    MDI = mdi, 
+    MDA = mda,
+    DE = de_idx,
+    Run = b
+  )
+  
+}
+sims <- foreach(b = seq_len(10), .combine = rbind) %dopar% loop(b) 
+saveRDS(sims, './Results/Simulations/RNA_seq.rds')
+
+# Strobl et al. simulation
 sigma <- matrix(0, nrow = 12, ncol = 12)
 sigma[1:4, 1:4] <- 0.9
 diag(sigma) <- 1
 x <- mvrnorm(n = 100 * 12, mu = rep(0, 12), Sigma = sigma)
 colnames(x) <- paste0('x', 1:12)
-beta <- c(5, 5, 2, 0, -5, -5, -2, 0, 0, 0, 0, 0)
+beta <- c(5, 5, 2, 0, -5, -5, -2, rep(0, times = 5))
 y <- drop(tcrossprod(beta, x)) + rnorm(100, mean = 0, sd = 0.5)
-out <- c_imp(x, y)
 
-# Compare with LMG method (ground truth for linear model?)
-m <- lm(y ~ x)
-lmg <- calc.relimp(m, type = 'lmg')
+
+
+
