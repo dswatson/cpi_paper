8646dee030a10daaaea26ad76879d255909c524b
dswatson <david.s.watson11@gmail.com>
2018-04-11 16:00:12 +0200

    low-d tests

--- tests.R
+++ tests.R
@@ -6,7 +6,7 @@
 ### Also worth running Altmann permutations to see p-value differences?
 
 # Load libraries, register cores
-library(mlbech)
+library(mlbench)
 library(ranger)
 library(party)
 library(matrixStats)
@@ -20,23 +20,27 @@ source('cpi.R')
 
 eval <- function(dat, x, y, type, high, runs) {
   
-  # Prelimz
+  # Preliminaries
   p <- ncol(x)
-  df <- ifelse(type == 'regression', data.frame(x, y = y), 
-               data.frame(x, y = as.factor(y)))
-  mtry <- ifelse(type == 'regression', floor(p/3), floor(sqrt(p)))
+  if (type %in% c('regression', 'survival')) {
+    df <- data.frame(x, y = y)
+    mtry <- floor(p / 3)
+  } else {
+    df <- data.frame(x, y = as.factor(y))
+    mtry <- floor(sqrt(p))
+  }
   B <- ifelse(high, 1e4L, 1e3L)
-  ctrl <- cforest_control(teststat = 'quad', testtype = 'Univ',
-                          mtry = mtry, ntree = B, replace = FALSE)
+  ctrl <- cforest_unbiased(mtry = mtry, ntree = B)
   
   # Brute force algorithm
   loop <- function(b) {
-    brute_force(x, y, type = type, weights = FALSE, 
-                replace = FALSE, mtry = mtry, B = B,
-                n.cores = 1, seed = b) %>%
-      rename(VI = Delta) %>%
-      mutate(Rank = min_rank(desc(VI)), VIM = 'CPI') %>%
-      select(Feature, VIM, VI, Rank)
+    imp <- brute_force(x, y, type = type, test = NULL, weights = FALSE, 
+                       replace = FALSE, mtry = mtry, B = B,
+                       n.cores = 1, seed = b)
+    data.frame(Feature = names(imp),
+                   VIM = 'CPI',
+                    VI = as.numeric(imp)) %>%
+      mutate(Rank = min_rank(desc(VI))) 
   }
   cpi_out <- foreach(b = seq_len(runs), .combine = rbind) %dopar% loop(b) 
   
@@ -45,7 +49,7 @@ eval <- function(dat, x, y, type, high, runs) {
     f <- ranger(data = df, dependent.variable.name = 'y',
                 mtry = mtry, num.trees = B, replace = FALSE, 
                 importance = 'impurity', num.threads = 1, seed = b)
-    imp <- importance(rf)
+    imp <- importance(f)
     data.frame(Feature = names(imp), 
                    VIM = 'MDI',
                     VI = as.numeric(imp)) %>%
@@ -58,7 +62,7 @@ eval <- function(dat, x, y, type, high, runs) {
     f <- ranger(data = df, dependent.variable.name = 'y',
                 mtry = mtry, num.trees = B, replace = FALSE, 
                 importance = 'permutation', num.threads = 1, seed = b)
-    imp <- importance(rf)
+    imp <- importance(f)
     data.frame(Feature = names(imp), 
                    VIM = 'MDA',
                     VI = as.numeric(imp)) %>%
@@ -82,50 +86,16 @@ eval <- function(dat, x, y, type, high, runs) {
   rbind(cpi_out, mdi_out, mda_out, strobl_out) %>%
     saveRDS(paste0('./Results/', dat, '_vim.rds'))
   
-  # Timing?  
-  mdi <- function(df, mtry, B, par) {
-    f <- ranger(data = df, dependent.variable.name = 'y', 
-                mtry = mtry, num.trees = B, replace = FALSE,
-                importance = 'impurity', num.threads = ifelse(par, 4, 1))
-    importance(f)
-  }
-  mda <- function(df, mtry, B, par) {
-    f <- ranger(data = df, dependent.variable.name = 'y',
-                mtry = mtry, num.trees = B, replace = FALSE,
-                importance = 'permutation', num.threads = ifelse(par, 4, 1))
-    importance(f)
-  }
-  strobl <- function(df, mtry, B) {  
-    f <- cforest(y ~ ., data = df, controls = ctrl)
-    varimp(f, conditional = TRUE)
-  }
-  times <- microbenchmark(
-    bf_par = brute_force(x, y, type = type, weights = FALSE,
-                         replace = FALSE, mtry = mtry, B = B, 
-                         n.cores = 4, seed = NULL),
-    bf_ser = brute_force(x, y, type = type, weights = FALSE,
-                         replace = FALSE, mtry = mtry, B = B, 
-                         n.cores = 1, seed = NULL),
-    mdi_par = mdi(df, mtry, B, par = TRUE),
-    mdi_ser = mdi(df, mtry, B, par = FALSE),
-    mda_par = mda(df, mtry, B, par = TRUE),
-    mda_ser = mda(df, mtry, B, par = FALSE),
-    strobl - strobl(df, mtry, B),
-    times = runs, unit = 's', control = list(warmup = 5)
-  )
-  data.frame(VIM = times$expr,
-            Time = times$time) %>%
-    saveRDS(paste0('./Results/', dat, '_times.rds'))
+  
   
 }
-
 
 
 # Test internal consistency
 vim <- readRDS('./Results/BostonHousing_vim.rds')
 cpi_res <- vim %>%
   filter(VIM == 'CPI') %>%
-  mutatee(Run = rep(seq_len(100), each = p))
+  mutate(Run = rep(seq_len(100), each = p))
 mat <- matrix(nrow = 100, ncol = 100,
               dimnames = list(paste0('r', seq_len(100)), 
                               paste0('r', seq_len(100))))
@@ -137,6 +107,7 @@ for (i in 2:100) {
   }
 }
 mean(mat, na.rm = TRUE)
+
 # VIM consistency
 for (i in 2:100) {
   for (j in 1:(i - 1)) {
