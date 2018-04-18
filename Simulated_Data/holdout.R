6701eabf7d9f862451da39e6d9bfb4ec8a56041f
dswatson <david.s.watson11@gmail.com>
2018-04-18 18:52:35 +0200

    hold out > OOB?

---
+++ Simulated_Data/holdout.R
@@ -0,0 +1,83 @@
+### HOLD OUT ###
+
+# Load libraries, register cores
+library(ranger)
+library(matrixStats)
+library(tidyverse)
+library(doMC)
+registerDoMC(20)
+
+# Simulation function
+sim <- function(b, n, p) {
+  
+  # Simulate data
+  set.seed(b)
+  x <- matrix(runif(n * p), ncol = p,
+              dimnames = list(NULL, paste0('x', seq_len(p))))
+  y <- rnorm(n)
+  df <- data.frame(x, y = y)
+  split <- sample.int(n, n / 2)
+  df1 <- df[split, ]
+  df2 <- df[-split, ]
+  rm(x, y, df)
+  
+  # Grow forests, calculate predictions and loss
+  mtry <- floor(sqrt(p))
+  B <- 2 * p
+  rf1 <- ranger(data = df1, dependent.variable.name = 'y', 
+                mtry = mtry, num.trees = B, replace = TRUE,
+                num.threads = 20, seed = b)
+  rf2 <- ranger(data = df2, dependent.variable.name = 'y', 
+                mtry = mtry, num.trees = B, replace = TRUE,
+                num.threads = 20, seed = b)
+  preds1 <- predict(rf1, data = df2, num.threads = 20, predict.all = TRUE)
+  preds2 <- predict(rf2, data = df1, num.threads = 20, predict.all = TRUE)
+  y_hat1 <- rowMeans2(preds1)
+  y_hat2 <- rowMeans2(preds2)
+  loss1 <- (df2$y - y_hat1)^2
+  loss2 <- (df1$y - y_hat2)^2
+  
+  # Drop function
+  drop <- function(j) {
+    f0_idx1 <- !rf1$forest$variable.selected[, j]
+    y_hat01 <- rowMeans2(preds1[, f0_idx1, drop = FALSE])
+    loss01 <- (df2$y - y_hat01)^2
+    f0_idx2 <- !rf2$forest$variable.selected[, j]
+    y_hat02 <- rowMeans2(preds2[, f0_idx2, drop = FALSE])
+    loss02 <- (df1$y - y_hat02)^2
+    loss0 <- (loss01 + loss02) / 2
+    t_test <- t.test(loss0, loss, paired = TRUE, alternative = 'greater')
+    out <- c(t_test$estimate, t_test$estimate / t_test$statistic,
+             t_test$statistic, t_test$p.value)
+    return(out)
+  }
+  
+  # Execute in parallel, export
+  delta <- foreach(j = seq_len(p), .combine = rbind) %dopar% drop(j)
+  dimnames(delta) <- list(NULL, c('CPI', 'SE', 't', 'p.value'))
+  delta <- data.frame(Feature = paste0('x', seq_len(p)), delta)
+  return(delta)
+  
+}
+
+# Run 10 times
+n <- 100
+p <- 5000
+delta <- foreach(b = seq_len(10), .combine = rbind) %do% sim(b, n, p)
+
+# What proportion of p-values are <= 0.05?
+p_props <- sapply(seq_len(10), function(b) {
+  sum(delta$p.value[delta$Run == b] <= 0.05) / p
+})
+p_props
+
+# Let's see those t-statistics
+ggplot(delta, aes(t)) + 
+  geom_histogram(aes(y = ..density..), bins = 100, color = 'black') + 
+  stat_function(fun = dt, color = 'red', args = list(df = n - 1)) + 
+  theme_bw()
+
+
+
+
+
