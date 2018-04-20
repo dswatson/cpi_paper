c556ce411961f03793c87d7d5f8fc9c70a60cfff
dswatson <david.s.watson11@gmail.com>
2018-04-20 16:02:34 +0100

    CPI vs random subsets

---
+++ Simulated_Data/cpi_rdm_subset.R
@@ -0,0 +1,73 @@
+### CPI vs. collection of random forest subsets ###
+
+# Load libraries, register cores
+library(ranger)
+library(matrixStats)
+library(tidyverse)
+library(doMC)
+registerDoMC(20)
+
+# Simulation function
+sim <- function(b, n, p) {
+  
+  set.seed(b)
+  x <- matrix(runif(n * p), ncol = p,
+              dimnames = list(NULL, paste0('x', seq_len(p))))
+  y <- rnorm(n)
+  
+  # Grow forest
+  mtry <- floor(sqrt(p))
+  B <- 2 * p
+  df <- data.frame(x, y = y)
+  rf <- ranger(data = df, dependent.variable.name = 'y', 
+               mtry = mtry, num.trees = B, replace = FALSE,
+               keep.inbag = TRUE, num.threads = 20, seed = b)
+  
+  # Create oob_preds object
+  oob_idx <- ifelse(simplify2array(rf$inbag.counts) == 0, TRUE, NA)
+  preds <- predict(rf, df, num.threads = 20, predict.all = TRUE)$predictions
+  oob_preds <- oob_idx * preds
+  
+  # How many trees expected in sub-forest?
+  splits <- foreach(b = seq_len(B), .combine = c) %dopar% 
+    sum(!treeInfo(rf, b)$terminal)
+  exp_B0 <- round(B * (1 - mtry/p)^mean(splits))
+  
+  # Random sub-forests
+  loop <- function(j) {
+    set.seed(j)
+    sub_idx <- sample.int(B, exp_B0)
+    y_hat <- rowMeans2(oob_preds[, sub_idx, drop = FALSE], na.rm = TRUE)
+    loss <- (y_hat - y)^2
+    return(loss)
+  }
+  loss <- rowMeans2(foreach(j = seq_len(100), .combine = cbind) %dopar% loop(j))
+  
+  # Null sub-forests
+  drop <- function(j) {
+    f0_idx <- !rf$forest$variable.selected[, j]
+    y_hat0 <- rowMeans2(oob_preds[, f0_idx, drop = FALSE], na.rm = TRUE)
+    loss0 <- (y_hat0 - y)^2
+    t_test <- t.test(loss0, loss, paired = TRUE, alternative = 'greater')
+    out <- c(t_test$estimate, t_test$estimate / t_test$statistic,
+             t_test$statistic, t_test$p.value)
+  }
+  delta <- foreach(j = seq_len(p), .combine = rbind) %dopar% drop(j)
+  dimnames(delta) <- list(NULL, c('CPI', 'SE', 't', 'p.value'))
+  delta <- data.frame(Feature = colnames(x), delta)
+  return(delta)
+  
+}
+
+# Simulate data
+n <- 100
+p <- 5000
+out <- sim(1, n, p)
+
+  
+  
+  
+  
+  
+  
+  
