a284ad513310be0d8b0f639a16c19ae9350953e2
Marvin Wright <github@wrig.de>
2018-05-14 16:11:26 +0200

    use measure in mlr, add LRT

--- cpi_mlr.R
+++ cpi_mlr.R
@@ -3,38 +3,66 @@ library(mlr)
 library(foreach)
 
 # TODO: Add confidence intervals
-# TODO: How to compute loss for classification?
 brute_force_mlr <- function(task, learner, 
                             resampling = makeResampleDesc("CV", iters = 5), 
+                            measure = NULL,
                             test = NULL,
                             verbose = FALSE, 
                             cores = 1) {
+  if (is.null(measure)) {
+    if (getTaskType(task) == "regr") {
+      measure <- mse
+    } else if (getTaskType(task) == "classif") {
+      measure <- logloss
+    } else {
+      stop("Unknown task type.")
+    }
+  }
+  
+  if (!is.null(test)) {
+    if (!(measure$id %in% c("mse", "logloss"))) {
+      stop("Statistical testing currently only implemented for 'mse' and 'logloss' measure.")
+    }
+  }
   
   # Full model resampling
-  full_model <- resample(learner, task, resampling = resampling, show.info = verbose)
-  err_full <- compute_loss(full_model)
+  full_model <- resample(learner, task, resampling = resampling, measure = measure, show.info = verbose)
+  if (!is.null(test)) {
+    err_full <- compute_loss(full_model)
+  }
   
   # For each feature, fit reduced model and return difference in error
   cpi_fun <- function(i) {
     reduced_task <- subsetTask(task, features = getTaskFeatureNames(task)[-i])
-    reduced_model <- resample(learner, reduced_task, resampling = resampling, show.info = verbose)
-    err_reduced <- compute_loss(reduced_model)
+    reduced_model <- resample(learner, reduced_task, resampling = resampling, measure = measure, show.info = verbose)
     
     res <- data.frame(Variable = getTaskFeatureNames(task)[i],
-                      CPI = mean(err_reduced - err_full), 
+                      CPI = unname(reduced_model$aggr - full_model$aggr), 
                       stringsAsFactors = FALSE)
 
     # Statistical testing
     if (!is.null(test)) {
+      err_reduced <- compute_loss(reduced_model)
       if (test == "t") {
         test_result <- t.test(err_reduced, err_full, paired = TRUE, alternative = 'greater')
-      } else if (test == "wilcox") {
-        test_result <- wilcox.test(err_reduced, err_full, paired = TRUE, alternative = 'greater')
+        res$statistic <- test_result$statistic
+        res$p.value <- test_result$p.value
+      } else if (test == "lrt") {
+        if (measure$id == "mse") {
+          ll_full <- sum(log(dnorm(full_model$pred$data$truth, mean = full_model$pred$data$response, sd = sqrt(full_model$aggr))))
+          ll_reduced <- sum(log(dnorm(reduced_model$pred$data$truth, mean = reduced_model$pred$data$response, sd = sqrt(reduced_model$aggr))))
+        } else if (measure$id == "logloss") {
+          ll_full <- sum(-err_full)
+          ll_reduced <- sum(-err_reduced)
+        } else {
+          stop("Likelihood ratio test not implemented for this measure.")
+        }
+        d <- 2 * (ll_full - ll_reduced)
+        res$statistic <- d
+        res$p.value <- pchisq(d, df = 1, lower = FALSE)
       } else {
         stop("Unknown test.")
       }
-      res$statistic <- test_result$statistic
-      res$p.value <- test_result$p.value
     }
     res
   }
@@ -51,12 +79,19 @@ compute_loss <- function(model) {
   if (getTaskType(model) == "regr") {
     # MSE
     loss <- (model$pred$data$truth - model$pred$data$response)^2
-  } else if (getTaskType(task) == "classif") {
+  } else if (getTaskType(model) == "classif") {
     if (hasLearnerProperties(model$learner, "prob")) {
-      # 0/1 loss
-      y <- model$pred$data$truth
-      y_hat <- model$pred$data$response
-      loss <- -(y * log(y_hat) + (1 - y) * log(1 - y_hat))
+      # logloss (taken from mlr::measureLogloss)
+      probabilities <- model$pred$data[, paste("prob", model$pred$task.desc$class.levels, sep = ".")]
+      
+      #let's confine the predicted probabilities to [eps,1 - eps], so logLoss doesn't reach infinity under any circumstance
+      eps <- 1e-15
+      probabilities[probabilities > 1 - eps] <- 1 - eps
+      probabilities[probabilities < eps] <- eps
+      
+      truth <- match(as.character(model$pred$data$truth), model$pred$task.desc$class.levels)
+      p <- mlr:::getRowEls(probabilities, truth)
+      loss <- -log(p)
     } else {
       stop("For classification the learner requires probability support.")
     }
@@ -65,3 +100,4 @@ compute_loss <- function(model) {
   }
   loss
 }
+
---
+++ test_mlr.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+library(tidyr)
+
+source("cpi_mlr.R")
+
+# Regression
+brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"))
+brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"), measure = mae)
+brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"), test = "t")
+brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"), test = "lrt")
+
+# Classification (binary)
+brute_force_mlr(task = pid.task, learner = makeLearner("classif.logreg", predict.type = "prob"))
+brute_force_mlr(task = pid.task, learner = makeLearner("classif.logreg", predict.type = "prob"), measure = mmce)
+brute_force_mlr(task = pid.task, learner = makeLearner("classif.logreg", predict.type = "prob"), test = "t")
+brute_force_mlr(task = pid.task, learner = makeLearner("classif.logreg", predict.type = "prob"), test = "lrt")
+
+# Classification (multiclass)
+brute_force_mlr(task = iris.task, learner = makeLearner("classif.glmnet", predict.type = "prob"))
+brute_force_mlr(task = iris.task, learner = makeLearner("classif.glmnet", predict.type = "prob"), measure = mmce)
+brute_force_mlr(task = iris.task, learner = makeLearner("classif.glmnet", predict.type = "prob"), test = "t")
+brute_force_mlr(task = iris.task, learner = makeLearner("classif.glmnet", predict.type = "prob"), test = "lrt")
+
