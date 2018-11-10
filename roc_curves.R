82daa3e37f907910ccf0d771c1fd6cef249b4c44
dswatson <david.s.watson11@gmail.com>
2018-11-11 00:45:35 +0100

    pcor

--- roc_curves.R
+++ roc_curves.R
@@ -12,13 +12,15 @@ library(ggsci)
 library(doMC)
 registerDoMC(4)
 
+# Set seed
+set.seed(123)
+
 # Friedman1 benchmark hyperparameters
 n <- 100
 p <- 10
 
 # These will come up a lot
 models <- c('LM', 'RF', 'NN', 'SVM')
-vims <- c('CPI', 'ANOVA', 'LRT')
 
 # Loss function
 compute_loss <- function(trn, tst) {
@@ -26,31 +28,33 @@ compute_loss <- function(trn, tst) {
   # Linear model
   lm_fit <- lm(y ~ ., data = trn)
   lm_yhat <- predict(lm_fit, newdata = tst)
-  lm_loss <- (tst$y - lm_yhat)^2
+  lm_eps <- tst$y - lm_yhat
   
   # Random forest
   rf_fit <- ranger(y ~ ., data = trn, num.trees = 50)
   rf_yhat <- predict(rf_fit, tst)$predictions
-  rf_loss <- (tst$y - rf_yhat)^2
+  rf_eps <- tst$y - rf_yhat
   
   # Neural network
   nn_fit <- nnet(y ~ ., data = trn, size = 3, decay = 0.2, 
                  linout = TRUE, trace = FALSE)
   nn_yhat <- predict(nn_fit, newdata = tst)
-  nn_loss <- (tst$y - nn_yhat)^2
+  nn_eps <- tst$y - nn_yhat
   
   # Support vector machine
   svm_fit <- svm(y ~ ., data = trn, kernel = 'radial', fitted = FALSE)
   svm_yhat <- predict(svm_fit, newdata = tst)
-  svm_loss <- (tst$y - svm_yhat)^2
+  svm_eps <- tst$y - svm_yhat
   
   # Export
   out <- data.frame(
-    lm_yhat, lm_loss, rf_yhat, rf_loss, 
-    nn_yhat, nn_loss, svm_yhat, svm_loss
-  )
+    lm_yhat, lm_eps, rf_yhat, rf_eps, 
+    nn_yhat, nn_eps, svm_yhat, svm_eps
+  ) %>%
+    mutate(lm_loss = lm_eps^2, rf_loss = rf_eps^2,
+           nn_loss = nn_eps^2, svm_loss = svm_eps^2)
   return(out)
-    
+  
 }
 
 # Analysis loop
@@ -77,8 +81,12 @@ loop <- function(b) {
     out <- data.table(
       'Model' = models,
       'Feature' = j,
-      'VIM' = 'cpi',
+      'VIM' = rep(c('delta', 'lambda'), each = length(models)),
       'VI' = c(
+        mean(null$lm_loss - orig$lm_loss),
+        mean(null$rf_loss - orig$rf_loss),
+        mean(null$nn_loss - orig$nn_loss),
+        mean(null$svm_loss - orig$svm_loss),
         mean(log(null$lm_loss / orig$lm_loss)),
         mean(log(null$rf_loss / orig$rf_loss)),
         mean(log(null$nn_loss / orig$nn_loss)),
@@ -148,9 +156,38 @@ loop <- function(b) {
     )
   }
   anova_df <- foreach(j = seq_len(p), .combine = rbind) %do% anova_fn(j)
-
+  
+  ### Ramsey's conditional correlation independence (CCI) test and ###
+  ### Shah & Peter's generalised covariance measure (GCM) test     ###
+  pcor_fn <- function(j) {
+    f <- compute_loss(train[, -j], test[, -j])
+    train0 <- select(train, -y)
+    colnames(train0)[j] <- 'y'
+    test0 <- select(test, -y)
+    colnames(test0)[j] <- 'y'
+    g <- compute_loss(train0, test0)
+    r_lm <- f$lm_eps * g$lm_eps
+    r_rf <- f$rf_eps * g$rf_eps
+    r_nn <- f$nn_eps * g$nn_eps
+    r_svm <- f$svm_eps * g$svm_eps
+    data.table(
+      'Model' = rep(models, times = 2),
+      'Feature' = j,
+      'VIM' = rep(c('cci', 'gcm'), each = 4),
+      'VI' = c(
+        cor(f$lm_eps, g$lm_eps)^2, cor(f$rf_eps, g$rf_eps)^2,
+        cor(f$nn_eps, g$nn_eps)^2, cor(f$svm_eps, g$svm_eps)^2,
+        abs((sqrt(n) * mean(r_lm)) / sqrt(mean(r_lm^2) - (mean(r_lm))^2)),
+        abs((sqrt(n) * mean(r_rf)) / sqrt(mean(r_rf^2) - (mean(r_rf))^2)),
+        abs((sqrt(n) * mean(r_nn)) / sqrt(mean(r_nn^2) - (mean(r_nn))^2)),
+        abs((sqrt(n) * mean(r_svm)) / sqrt(mean(r_svm^2) - (mean(r_svm))^2))
+      )
+    )
+  }
+  pcor_df <- foreach(j = seq_len(p), .combine = rbind) %do% pcor_fn(j)
+  
   # Put it all together
-  out <- rbind(cpi_df, anova_df, lrt_df)
+  out <- rbind(cpi_df, anova_df, lrt_df, pcor_df)
   out[, Run := b]
   return(out)
   
@@ -162,9 +199,12 @@ out <- foreach(b = seq_len(100), .combine = rbind) %dopar% loop(b)
 # Prepare to plot
 obs <- rep(rep(c(1, 0), each = 5), times = 100)
 auroc <- function(model) {
-  pred <- list(CPI = out[VIM == 'cpi' & Model == model, VI],
+  pred <- list(CPI_d = out[VIM == 'delta' & Model == model, VI],
+               CPI_l = out[VIM == 'lambda' & Model == model, VI],
                ANOVA = out[VIM == 'anova' & Model == model, VI],
-               LRT = out[VIM == 'lrt' & Model == model, VI])
+               LRT = out[VIM == 'lrt' & Model == model, VI],
+               CCI = out[VIM == 'cci' & Model == model, VI],
+               GCM = out[VIM == 'gcm' & Model == model, VI])
   rocs <- evalmod(scores = pred, labels = obs)$rocs
   df <- seq_along(pred) %>%
     map_df(~ data_frame(FPR = rocs[[.x]]$x,
@@ -185,7 +225,12 @@ plot_fn <- function(model) {
     unlist(.) %>%
     as.numeric(.) %>%
     round(., 3)
-  lbls <- paste0(vims, ', AUC = ', auc_vals)
+  lbls <- c(bquote('CPI'[Delta]*', AUC = '~.(auc_vals[1])),
+            bquote('CPI'[lambda]*', AUC = '~.(auc_vals[2])),
+            paste0('ANOVA, AUC = ', auc_vals[3]),
+            paste0('LRT, AUC = ', auc_vals[4]),
+            paste0('CCI, AUC = ', auc_vals[5]),
+            paste0('GCM, AUC = ', auc_vals[6]))
   if (model == 'LM') {
     title <- 'Linear Model'
   } else if (model == 'RF') {
@@ -201,9 +246,9 @@ plot_fn <- function(model) {
     geom_abline(intercept = 0, slope = 1,
                 linetype = 'dashed', color = 'black') +
     scale_color_manual(name = 'VIM',
-                       breaks = vims,
+                       breaks = unique(df$VIM),
                        labels = lbls,
-                       values = pal_d3()(3)) + 
+                       values = pal_d3()(6)) + 
     lims(x = c(0, 1), y = c(0, 1)) +
     labs(title = title, 
          x = 'False Positive Rate', 
@@ -220,16 +265,5 @@ p_nn <- plot_fn('NN')
 p_svm <- plot_fn('SVM')
 grid.arrange(p_lm, p_rf, p_nn, p_svm, ncol = 2)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Remove AUC values from plots, create table with AUC and associated SE
 
