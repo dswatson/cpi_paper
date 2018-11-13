f0d2e19474b9ab83f423dd57381766f31b5291a3
Marvin Wright <github@wrig.de>
2018-11-13 15:35:14 +0100

    update boston housing example

--- Real_Data/boston_housing.R
+++ Real_Data/boston_housing.R
@@ -7,39 +7,36 @@ source("../cpi_mlr.R")
 
 seed <- 100
 
+# Linear model
 set.seed(seed)
 cpi_lm_log <- brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"), 
-                              resampling = makeResampleDesc("CV", iters = 10), 
+                              resampling = makeResampleDesc("Subsample", iters = 20), 
                               test = "t", measure = mse, log = TRUE)
 
-set.seed(seed)
-cpi_lm_dif <- brute_force_mlr(task = bh.task, learner = makeLearner("regr.lm"), 
-                              resampling = makeResampleDesc("CV", iters = 10), 
-                              test = "t", measure = mse, log = FALSE)
-
+# SVM
 set.seed(seed)
 cpi_svm_log <- brute_force_mlr(task = bh.task, learner = makeLearner("regr.svm", kernel = "radial"), 
-                               resampling = makeResampleDesc("CV", iters = 10), 
+                               resampling = makeResampleDesc("Subsample", iters = 20), 
                                test = "t", measure = mse, log = TRUE)
 
-set.seed(seed)
-cpi_svm_dif <- brute_force_mlr(task = bh.task, learner = makeLearner("regr.svm", kernel = "radial"), 
-                               resampling = makeResampleDesc("CV", iters = 10), 
-                               test = "t", measure = mse, log = FALSE)
-
+# Combine for plotting
 res <- rbind(data.table(Learner = "Linear model", Log = "Multiplicative CPI", cpi_lm_log[, c("Variable", "CPI", "SE", "p.value")]),
-             data.table(Learner = "Linear model", Log = "Additive CPI", cpi_lm_dif[, c("Variable", "CPI", "SE", "p.value")]), 
-             data.table(Learner = "Support vector machine", Log = "Multiplicative CPI", cpi_svm_log[, c("Variable", "CPI", "SE", "p.value")]), 
-             data.table(Learner = "Support vector machine", Log = "Additive CPI", cpi_svm_dif[, c("Variable", "CPI", "SE", "p.value")]))
-res[, signif := ifelse(p.value <= .05, "*", "")]
+             data.table(Learner = "Support vector machine", Log = "Multiplicative CPI", cpi_svm_log[, c("Variable", "CPI", "SE", "p.value")]))#, 
+res[, p.adj := p.adjust(p.value, "holm")]
+res[, signif := ifelse(p.adj <= .05, 1, 0)]
+levels <- res[Learner == "Support vector machine", as.character(Variable)[order(CPI)]]
+res[, Variable := factor(Variable, levels = levels)]
 
-ggplot(res, aes(x = Variable, fill = Learner, y = CPI, label = signif)) + 
-  geom_bar(stat = "identity", position = position_dodge()) + 
-  #geom_errorbar(aes(ymin = CPI - SE, ymax = CPI + SE), position = position_dodge()) +
-  geom_text(size = 7, position = position_dodge(width = 1), hjust = 1, vjust = .8, angle = 180) + 
-  facet_wrap(~ Log, scales = "free") + 
+# Plot
+ggplot(res, aes(x = Variable, fill = Learner, y = CPI, alpha = signif)) + 
+  geom_bar(stat = "identity", position = position_dodge(-.9)) + 
+  geom_errorbar(aes(ymin = CPI - SE, ymax = CPI + SE), position = position_dodge(-.9)) +
+  #facet_wrap(~ Log, scales = "free") + 
   scale_fill_npg() +
+  scale_alpha_continuous(range = c(.4, 1)) + 
   coord_flip() + 
   theme_bw() + 
-  theme(legend.position = "top")
-ggsave("boston_housing.pdf", width = 8, height = 8)
+  theme(legend.position = "top") + 
+  guides(alpha = FALSE) + 
+  ylab(expression('CPI'[lambda]))
+ggsave("boston_housing.pdf", width = 6, height = 8)
