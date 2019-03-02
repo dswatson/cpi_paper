cfce1b9569659bc413822c454670466972b990c5
dswatson <david.watson@oii.ox.ac.uk>
2019-03-02 15:52:15 +0000

    ko_test

--- 4.1_Simulated_Data/4.1.2 Comparative Performance/comparative_performance.R
+++ 4.1_Simulated_Data/4.1.2 Comparative Performance/comparative_performance.R
@@ -1,5 +1,6 @@
 # Load libraries, register cores
 library(data.table)
+library(knockoff)
 library(ranger)
 library(nnet)
 library(e1071)
@@ -8,7 +9,7 @@ library(vimp)
 library(ggsci)
 library(tidyverse)
 library(doMC)
-registerDoMC(8)
+registerDoMC(20)
 
 # Set seed
 set.seed(123, kind = "L'Ecuyer-CMRG")
@@ -19,48 +20,13 @@ p <- 10
 # These will come up a lot
 models <- c('LM', 'RF', 'NN', 'SVM')
 
-# Loss function
-compute_loss <- function(trn, tst) {
-  
-  # Linear model
-  lm_fit <- lm(y ~ ., data = trn)
-  lm_yhat <- predict(lm_fit, newdata = tst)
-  lm_eps <- tst$y - lm_yhat
-  
-  # Random forest
-  rf_fit <- ranger(y ~ ., data = trn, num.trees = 50)
-  rf_yhat <- predict(rf_fit, tst)$predictions
-  rf_eps <- tst$y - rf_yhat
-  
-  # Neural network
-  nn_fit <- nnet(y ~ ., data = trn, size = 20, decay = 0.1, 
-                 linout = TRUE, trace = FALSE)
-  nn_yhat <- predict(nn_fit, newdata = tst)
-  nn_eps <- tst$y - nn_yhat
-  
-  # Support vector machine
-  svm_fit <- svm(y ~ ., data = trn, kernel = 'radial', fitted = FALSE)
-  svm_yhat <- predict(svm_fit, newdata = tst)
-  svm_eps <- tst$y - svm_yhat
-  
-  # Export
-  out <- data.frame(
-    lm_yhat, lm_eps, rf_yhat, rf_eps, 
-    nn_yhat, nn_eps, svm_yhat, svm_eps
-  ) %>%
-    mutate(lm_loss = lm_eps^2, rf_loss = rf_eps^2,
-           nn_loss = nn_eps^2, svm_loss = svm_eps^2)
-  return(out)
-  
-}
-
-loop <- function(b, cov_base, sim, test_n) {
+loop <- function(b, rho, sim, test_n) {
   
   # Simulate data
   beta <- c(0, 0, -0.5, 0.5, -1, 1, -1.5, 1.5, -2, 2)
-  sigma <- toeplitz(cov_base^(0:(p - 1)))
-  x <- matrix(rmvnorm(3 * test_n, sigma = sigma), ncol = p,
-              dimnames = list(NULL, paste0('x', seq_len(p))))
+  Sigma <- toeplitz(rho^(0:(p - 1)))
+  x <- matrix(rnorm(3 * test_n * p), ncol = p) %*% chol(Sigma)
+  dimnames(x) <- list(NULL, paste0('x', seq_len(p)))
   if (sim == 'linear') {
     y <- x %*% beta + rnorm(3 * test_n)
   } else if (sim == 'nonlinear') {
@@ -77,26 +43,53 @@ loop <- function(b, cov_base, sim, test_n) {
   train <- dat[idx, ]
   test <- dat[-idx, ]
   
-  # Fit and evaluate original models
-  orig <- compute_loss(train, test)
+  # Fit models
+  lm_fit <- lm(y ~ ., data = train)
+  rf_fit <- ranger(y ~ ., data = train)
+  nn_fit <- nnet(y ~ ., data = train, size = 20, decay = 0.1, 
+                 linout = TRUE, trace = FALSE)
+  svm_fit <- svm(y ~ ., data = train, kernel = 'radial', fitted = FALSE)
+  
+  # Compute predictions, residuals, and squared errors
+  lm_yhat <- predict(lm_fit, newdata = test)
+  lm_eps <- test$y - lm_yhat
+  lm_loss <- lm_eps^2
+  rf_yhat <- predict(rf_fit, test)$predictions
+  rf_eps <- test$y - rf_yhat
+  rf_loss <- rf_eps^2
+  nn_yhat <- predict(nn_fit, newdata = test)
+  nn_eps <- test$y - nn_yhat
+  nn_loss <- nn_eps^2
+  svm_yhat <- predict(svm_fit, newdata = test)
+  svm_eps <- test$y - svm_yhat
+  svm_loss <- svm_eps^2
+  
+  # Generate knockoff matrix
+  x_tilde <- create.second_order(x)
 
   ### CPI ###
   cpi_fn <- function(j) {
-    dat0[, j] <- dat[sample.int(3 * test_n), j]
-    train0 <- dat0[idx, ]
+    dat0[, j] <- x_tilde[, j]
     test0 <- dat0[-idx, ]
-    null <- compute_loss(train0, test0)
+    lm_yhat0 <- predict(lm_fit, newdata = test0)
+    rf_yhat0 <- predict(rf_fit, test0)$predictions
+    nn_yhat0 <- predict(nn_fit, newdata = test0)
+    svm_yhat0 <- predict(svm_fit, newdata = test0)
+    lm_loss0 <- (test$y - lm_yhat0)^2
+    rf_loss0 <- (test$y - rf_yhat0)^2
+    nn_loss0 <- (test$y - nn_yhat0)^2
+    svm_loss0 <- (test$y - svm_yhat0)^2
     delta <- data.table(
-      'LM' = null$lm_loss - orig$lm_loss,
-      'RF' = null$rf_loss - orig$rf_loss,
-      'NN' = null$nn_loss - orig$nn_loss,
-      'SVM' = null$svm_loss - orig$svm_loss
+      'LM' = lm_loss0 - lm_loss, 
+      'RF' = rf_loss0 - rf_loss,
+      'NN' = nn_loss0 - nn_loss, 
+      'SVM' = svm_loss0 - svm_loss
     )
     lambda <- data.table(
-      'LM' = log(null$lm_loss / orig$lm_loss),
-      'RF' = log(null$rf_loss / orig$rf_loss),
-      'NN' = log(null$nn_loss / orig$nn_loss),
-      'SVM' = log(null$svm_loss / orig$svm_loss)
+      'LM' = log(lm_loss0 / lm_loss),
+      'RF' = log(rf_loss0 / rf_loss),
+      'NN' = log(nn_loss0 / nn_loss),
+      'SVM' = log(svm_loss0 / svm_loss)
     )
     out <- data.table(
       'Simulation' = sim,
@@ -126,29 +119,29 @@ loop <- function(b, cov_base, sim, test_n) {
   anova_fn <- function(j) {
     train0 <- test[, -j]
     # Linear model
-    train0$y <- orig$lm_yhat
+    train0$y <- lm_yhat
     lm_fit0 <- lm(y ~ ., data = train0)
     lm_yhat0 <- predict(lm_fit0, newdata = test)
-    vimp_lm <- vimp_regression(test$y, f1 = orig$lm_yhat, f2 = lm_yhat0, indx = j, 
+    vimp_lm <- vimp_regression(test$y, f1 = lm_yhat, f2 = lm_yhat0, indx = j, 
                                run_regression = FALSE)
     # Random forest
-    train0$y <- orig$rf_yhat
+    train0$y <- rf_yhat
     rf_fit0 <- ranger(y ~ ., data = train0, num.trees = 50)
     rf_yhat0 <- predict(rf_fit0, data = test)$predictions
-    vimp_rf <- vimp_regression(test$y, f1 = orig$rf_yhat, f2 = rf_yhat0, indx = j, 
+    vimp_rf <- vimp_regression(test$y, f1 = rf_yhat, f2 = rf_yhat0, indx = j, 
                                run_regression = FALSE)
     # Neural network
-    train0$y <- orig$nn_yhat
+    train0$y <- nn_yhat
     nn_fit0 <- nnet(y ~ ., data = train0, size = 20, decay = 0.1, 
                     linout = TRUE, trace = FALSE)
     nn_yhat0 <- predict(nn_fit0, newdata = test)
-    vimp_nn <- vimp_regression(test$y, f1 = orig$nn_yhat, f2 = nn_yhat0, indx = j, 
+    vimp_nn <- vimp_regression(test$y, f1 = nn_yhat, f2 = nn_yhat0, indx = j, 
                                run_regression = FALSE)
     # SVM
-    train0$y <- orig$svm_yhat
+    train0$y <- svm_yhat
     svm_fit0 <- svm(y ~ ., data = train0, kernel = 'radial', fitted = FALSE)
     svm_yhat0 <- predict(svm_fit0, newdata = test)
-    vimp_svm <- vimp_regression(test$y, f1 = orig$svm_yhat, f2 = svm_yhat0, indx = j, 
+    vimp_svm <- vimp_regression(test$y, f1 = svm_yhat, f2 = svm_yhat0, indx = j, 
                                 run_regression = FALSE)
     # Compute nonparametric ANOVA for each model
     data.table(
@@ -172,22 +165,61 @@ loop <- function(b, cov_base, sim, test_n) {
   ### Chalupka et al.'s fast conditional independence test (FCIT) and ###
   ### Shah & Peters's generalised covariance measure (GCM)            ###
   other_fn <- function(j) {
-    f <- compute_loss(train[, -j], test[, -j])
+    # Fit models
+    lm_fit0 <- lm(y ~ ., data = train[, -j])
+    rf_fit0 <- ranger(y ~ ., data = train[, -j])
+    nn_fit0 <- nnet(y ~ ., data = train[, -j], size = 20, decay = 0.1, 
+                   linout = TRUE, trace = FALSE)
+    svm_fit0 <- svm(y ~ ., data = train[, -j], kernel = 'radial', 
+                    fitted = FALSE)
+    # Compute predictions, residuals, and squared errors
+    lm_yhat0 <- predict(lm_fit0, newdata = test[, -j])
+    lm_eps0 <- test$y - lm_yhat0
+    lm_loss0 <- lm_eps0^2
+    rf_yhat0 <- predict(rf_fit0, test[, -j])$predictions
+    rf_eps0 <- test$y - rf_yhat0
+    rf_loss0 <- rf_eps0^2
+    nn_yhat0 <- predict(nn_fit0, newdata = test[, -j])
+    nn_eps0 <- test$y - nn_yhat0
+    nn_loss0 <- nn_eps0^2
+    svm_yhat0 <- predict(svm_fit0, newdata = test[, -j])
+    svm_eps0 <- test$y - svm_yhat0
+    svm_loss0 <- svm_eps0^2
+    # FCIT
     delta <- data.table(
-      'LM' = f$lm_loss - orig$lm_loss,
-      'RF' = f$rf_loss - orig$rf_loss,
-      'NN' = f$nn_loss - orig$nn_loss,
-      'SVM' = f$svm_loss - orig$svm_loss
+      'LM' = lm_loss0 - lm_loss,
+      'RF' = rf_loss0 - rf_loss,
+      'NN' = nn_loss0 - nn_loss,
+      'SVM' = svm_loss0 - svm_loss
     )
+    # GCM
     train0 <- select(train, -y)
     colnames(train0)[j] <- 'y'
     test0 <- select(test, -y)
     colnames(test0)[j] <- 'y'
-    g <- compute_loss(train0, test0)
-    r_lm <- f$lm_eps * g$lm_eps
-    r_rf <- f$rf_eps * g$rf_eps
-    r_nn <- f$nn_eps * g$nn_eps
-    r_svm <- f$svm_eps * g$svm_eps
+    # Fit models
+    lm_fit_g <- lm(y ~ ., data = train0)
+    rf_fit_g <- ranger(y ~ ., data = train0)
+    nn_fit_g <- nnet(y ~ ., data = train0, size = 20, decay = 0.1, 
+                    linout = TRUE, trace = FALSE)
+    svm_fit_g <- svm(y ~ ., data = train0, kernel = 'radial', fitted = FALSE)
+    # Compute predictions, residuals, and squared errors
+    lm_yhat_g <- predict(lm_fit_g, newdata = test0)
+    lm_eps_g <- test$y - lm_yhat_g
+    lm_loss_g <- lm_eps_g^2
+    rf_yhat_g <- predict(rf_fit_g, test0)$predictions
+    rf_eps_g <- test$y - rf_yhat_g
+    rf_loss_g <- rf_eps_g^2
+    nn_yhat_g <- predict(nn_fit_g, newdata = test0)
+    nn_eps_g <- test$y - nn_yhat_g
+    nn_loss_g <- nn_eps_g^2
+    svm_yhat_g <- predict(svm_fit_g, newdata = test0)
+    svm_eps_g <- test$y - svm_yhat_g
+    svm_loss_g <- svm_eps_g^2
+    r_lm <- lm_eps0 * lm_eps_g 
+    r_rf <- rf_eps0 * rf_eps_g
+    r_nn <- nn_eps0 * nn_eps_g
+    r_svm <- svm_eps0 * svm_eps_g
     gcm_lm <- abs((sqrt(test_n) * mean(r_lm)) / 
                     sqrt(mean(r_lm^2) - (mean(r_lm))^2))
     gcm_rf <- abs((sqrt(test_n) * mean(r_rf)) / 
@@ -223,6 +255,7 @@ loop <- function(b, cov_base, sim, test_n) {
     ][Model == 'RF', MSE := mean(orig$rf_loss)
     ][Model == 'NN', MSE := mean(orig$nn_loss)
     ][Model == 'SVM', MSE := mean(orig$svm_loss)
+    ][, rho := rho
     ][, Run := b
     ][, Size := paste0('n = ', 2 * test_n)]
   return(out)
@@ -231,10 +264,10 @@ loop <- function(b, cov_base, sim, test_n) {
 
 # Execute in parallel
 out <- foreach(b = seq_len(1e4), .combine = rbind) %:%
-  foreach(cov_base = c(0, 0.9)) %:%
+  foreach(rho = c(0, 0.5)) %:%
   foreach(sim = c('linear', 'nonlinear'), .combine = rbind) %:%
   foreach(test_n = c(50, 250, 500), .combine = rbind) %dopar% 
-  loop(b, sim, test_n)
+  loop(b, rho, sim, test_n)
 saveRDS(out, 'comp_big_sim.rds')
 
 # Empirical error rates
